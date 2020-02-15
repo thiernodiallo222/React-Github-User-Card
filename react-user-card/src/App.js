57aa9c29192ad4fc79a85a65727338e5eaea46c3
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-02-14 21:36:39 -0500

    js

--- react-user-card/src/App.js
+++ react-user-card/src/App.js
@@ -23,3 +23,26 @@ export default App;
 
 
 
+// axios.get("https://api.github.com/users/thiernodiallo222")
+//   .then((response) => {
+
+//     console.log(response);
+//     root.appendChild(myInfo(response.data));
+//   })
+//   .catch(error => {
+//     console.log("Cannot find data requested !", error);
+//   });
+
+
+// axios.get("https://api.github.com/users/thiernodiallo222/followers")
+
+//   .then(res => {
+//     console.log(res);
+//     res.data.forEach(element => {
+//       root.appendChild(myInfo(element));
+//     });
+    
+//   })
+//   .catch(error => {
+//     console.log("Cannot find data", error);
+//   });
